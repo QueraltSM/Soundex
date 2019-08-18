1b581d99773e34fd8172aec4d5474f0b43bec30e
QueraltSosaMompel <queraltsosa@hotmail.com>
2019-08-18 10:58:31 +0100

    Initial Commit

---
+++ Soundex/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Soundex
+//
+//  Created by Queralt Sosa Mompel on 18/8/19.
+//  Copyright © 2019 Queralt Sosa Mompel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
