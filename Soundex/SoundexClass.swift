3e1f27b62928a1d7698fe1b14ceb0e98082db92a
QueraltSosaMompel <queraltsosa@hotmail.com>
2019-08-19 12:09:10 +0100

    Version with refactoring

--- Soundex/SoundexClass.swift
+++ Soundex/SoundexClass.swift
@@ -30,14 +30,7 @@ class SoundexClass {
         if (letters5.contains(String(letter))) {
             return "5"
         }
-        if ("r" == String(letter)) {
-            return"6"
-        }
-        return ""
-    }
-    
-    func checkNext(letter: String, oldLetter: String) -> Bool {
-        return oldLetter == letter
+        return"6"
     }
     
     func removeFirstCoincidences() -> String {
@@ -50,25 +43,40 @@ class SoundexClass {
         return result
     }
     
+    func removeIndex(index: String.Index, offSet: Int) {
+         result.remove(at: result.index(index, offsetBy: offSet))
+    }
+    
     func removeXY(XY:String) {
         result = removeFirstCoincidences()
         while (result.contains(XY)) {
-            print("result = " + result)
             let index = result.firstIndex(of: Character(XY))
             if index != result.startIndex {
                 let before = result[result.index(index!, offsetBy: -1)]
                 let next = result[result.index(index!, offsetBy: +1)]
-                if before == next &&
-                    String(result[result.index(index!, offsetBy: 0)]) != "X" {
-                    result.remove(at: result.index(index!, offsetBy: +1))
+                if before == next && String(result[index!]) != "X" {
+                    removeIndex(index: index!,offSet: 1)
                 } else if String(result[result.index(index!, offsetBy: 0)]) != XY {
-                    result.remove(at: result.index(index!, offsetBy: 0))
-                } else if before == next &&
-                    String(result[result.index(index!, offsetBy: 0)]) == "X" {
+                    removeIndex(index: index!,offSet: 0)
+                } else if before == next && String(result[index!]) == "X" {
                     result += String(next)
                 }
             }
             result.remove(at: index!)
+        }
+    }
+    
+    func checkCases45(text: String) {
+        var count = 1
+        while (getNumber(letter: String(text.first!).lowercased()) == String(result.prefix(count))) {
+            result.removeFirst()
+            count += 1
+        }
+        if result.count > 2 && result.count > 3 {
+            result = String(result.prefix(3))
+        }
+        while result.count < 3 {
+            result += "0"
         }
     }
     
@@ -78,20 +86,7 @@ class SoundexClass {
         }
         removeXY(XY: "X")
         removeXY(XY: "Y")
-
-        var count = 1
-        while (getNumber(letter: String(text.first!).lowercased()) == String(result.prefix(count))) {
-            result.removeFirst()
-            count += 1
-        }
-        
-        if result.count > 2 && result.count > 3 {
-            result = String(result.prefix(3))
-        }
-
-        while result.count < 3 {
-            result += "0"
-        }
+        checkCases45(text: text)
         return text.prefix(1) + String(result)
     }
 }
